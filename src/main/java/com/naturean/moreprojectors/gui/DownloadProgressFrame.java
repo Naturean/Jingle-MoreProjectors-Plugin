dc27978f2ecffba40c870c596118953a52b799bd
Naturean <112891509+Naturean@users.noreply.github.com>
2025-05-02 21:49:22 +0800

    add plugin updater

---
+++ src/main/java/com/naturean/moreprojectors/gui/DownloadProgressFrame.java
@@ -0,0 +1,35 @@
+package com.naturean.moreprojectors.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Code from <a href="https://github.com/DuncanRuns/Julti/blob/main/src/main/java/xyz/duncanruns/julti/gui/DownloadProgressFrame.java">Julti</a>
+ * @author DuncanRuns
+ */
+public class DownloadProgressFrame extends JFrame {
+    private final JProgressBar bar;
+
+    public DownloadProgressFrame(Point location) {
+        this.setLayout(new GridBagLayout());
+        JLabel text = new JLabel("Downloading MoreProjectors Plugin...");
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        this.add(text, gbc);
+        this.bar = new JProgressBar(0, 100);
+        this.add(this.bar, gbc);
+
+
+        this.setSize(300, 100);
+        this.setTitle("MoreProjectors update");
+        this.setLocation(location);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public JProgressBar getBar() {
+        return this.bar;
+    }
+}
